d0a8a7fde5458fa49e2389daedb535417518d693
Yonathan Amare <70721412+yamare1@users.noreply.github.com>
2025-03-10 22:17:05 -0400

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,1 +1,28 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import pandas as pd
+import os
 
+class FoodDataset(Dataset):
+    """Dataset for food images"""
+    def __init__(self, annotations_file, img_dir, transform=None):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        
+    def __len__(self):
+        return len(self.img_labels)
+        
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = Image.open(img_path).convert('RGB')
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        return image, label
+
+def get_data_loaders(data_dir, annotations_file, batch_size=32, num_workers=4):
+    """Create train and validation data loaders"""
+    # Implementation would go here
+    return None, None  # Placeholder
